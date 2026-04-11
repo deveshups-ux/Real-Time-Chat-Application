2233aec6776e3a230283f12b27faf4b6cb56bf3c
DEVESH TIWARI <deveshups@gmail.com>
2026-04-11 15:04:33 +0530

    real time message transfer

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,5 +1,6 @@
 import { Message } from "../models/messageModel.js";
 import { Conversation } from "../models/conversationModel.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -10,26 +11,31 @@ export const sendMessage = async (req, res) => {
     let gotConversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     });
+
     if (!gotConversation) {
       gotConversation = await Conversation.create({
         participants: [senderId, receiverId],
       });
     }
+
     const newMessage = await Message.create({
       senderId,
       receiverId,
       message,
     });
+
     if (newMessage) {
       gotConversation.messages.push(newMessage._id);
     }
+
     await gotConversation.save();
 
-    return res.status(200).json({
-      newMessage,
-    });
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
-    // SOCKET IO
+    return res.status(200).json({ newMessage });
   } catch (error) {
     console.log(error);
   }
@@ -39,12 +45,15 @@ export const getMessage = async (req, res) => {
   try {
     const receiverId = req.params.id;
     const senderId = req.id;
+
     const conversation = await Conversation.findOne({
       participants: { $all: [senderId, receiverId] },
     }).populate("messages");
+
     if (!conversation) {
-      return res.status(200).json([]); 
+      return res.status(200).json([]);
     }
+
     return res.status(200).json(conversation.messages || []);
   } catch (error) {
     console.log(error);
---
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setMessages } from "../redux/messageSlice";
+
+const useGetRealTimeMessage = () => {
+  const { socket } = useSelector((store) => store.socket);
+  const { messages } = useSelector((store) => store.message);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      dispatch(setMessages([...messages, newMessage]));
+    });
+
+    return () => {
+      socket?.off("newMessage");
+    };
+  }, [socket, messages]);
+};
+
+export default useGetRealTimeMessage;
